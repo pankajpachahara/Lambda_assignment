707be29ca9de838311085cf2183d99a6e9604503
pankajpachahara <143988353+pankajpachahara@users.noreply.github.com>
2025-06-29 10:48:20 +0530

    Initial commit for automated Lambda deployment using Terraform

---
+++ index.js
@@ -0,0 +1,9 @@
+exports.handler = async (event) => {
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "text/plain"
+    },
+    body: "Hello from Node.js Lambda!",
+  };
+};
---
+++ main.tf
@@ -0,0 +1,130 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+# ------------------- NETWORKING -------------------
+
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_subnet" "subnet1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.subnet1_cidr
+  availability_zone = "${var.aws_region}a"
+}
+
+resource "aws_subnet" "subnet2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.subnet2_cidr
+  availability_zone = "${var.aws_region}b"
+}
+
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "rta1" {
+  subnet_id      = aws_subnet.subnet1.id
+  route_table_id = aws_route_table.rt.id
+}
+
+resource "aws_route_table_association" "rta2" {
+  subnet_id      = aws_subnet.subnet2.id
+  route_table_id = aws_route_table.rt.id
+}
+
+resource "aws_security_group" "alb_sg" {
+  name        = "alb-sg"
+  description = "Allow HTTP traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# ------------------- IAM + LAMBDA -------------------
+
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "lambda_exec_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Principal = { Service = "lambda.amazonaws.com" },
+      Effect = "Allow"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_lambda_function" "node_lambda" {
+  function_name    = "nodejs-lambda"
+  role             = aws_iam_role.lambda_exec_role.arn
+  handler          = "index.handler"
+  runtime          = "nodejs18.x"
+  filename         = "${path.module}/lambda.zip"
+  source_code_hash = filebase64sha256("${path.module}/lambda.zip")
+}
+
+# ------------------- ALB + Lambda Integration -------------------
+
+resource "aws_lb" "app_lb" {
+  name               = "lambda-alb"
+  internal           = false
+  load_balancer_type = "application"
+  subnets            = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
+  security_groups    = [aws_security_group.alb_sg.id]
+}
+
+resource "aws_lb_target_group" "lambda_tg" {
+  name        = "lambda-target"
+  target_type = "lambda"
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.app_lb.arn
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.lambda_tg.arn
+  }
+}
+
+resource "aws_lambda_permission" "alb_invoke" {
+  statement_id  = "AllowExecutionFromALB"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.node_lambda.function_name
+  principal     = "elasticloadbalancing.amazonaws.com"
+  source_arn    = aws_lb_target_group.lambda_tg.arn
+}
+
+resource "aws_lb_target_group_attachment" "lambda_attach" {
+  target_group_arn = aws_lb_target_group.lambda_tg.arn
+  target_id        = aws_lambda_function.node_lambda.arn
+}
