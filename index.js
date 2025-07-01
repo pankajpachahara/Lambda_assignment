3332ecc9dcd40b42adcbb03d13b627f6d6863860
Pankaj Pachahara <143988353+pankajpachahara@users.noreply.github.com>
2025-07-01 10:20:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ exports.handler = async (event) => {
     headers: {
       "Content-Type": "text/plain"
     },
-    body: "Hello from Node.js Lambda!",
+    body: "Hello from CloudTechner. This is the demo of Lambda_function",
   };
 };
 
