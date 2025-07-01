fa9a42ff0dcd8ac7ad0cd41ef1118e4e3be4cbc6
pankajpachahara <143988353+pankajpachahara@users.noreply.github.com>
2025-07-01 12:16:23 +0530

    Integrated S3 backend and updated Terraform configuration

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-bucket-pankaj-pachahara"  
+    key            = "lambda-alb/terraform.tfstate"   
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+}
+
 provider "aws" {
   region = var.aws_region
 }
---
+++ terraform-backend/main.tf
@@ -0,0 +1,38 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "terraform-state-bucket-pankaj-pachahara"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  tags = {
+    Name = "Terraform State Bucket"
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name = "Terraform Lock Table"
+  }
+}
